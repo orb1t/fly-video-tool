f9b667b322c8fa96fbda232ca8a2d6334ef0a62c
orb1t.ua <orb1t.ua@gmail.com>
2023-02-03 23:51:10 +0200

    updated README

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # fly-video-tool
+
+Run in Idea:
+1. "File" -> "New" -> "Project from Version Control..." : GIT : URL : https://github.com/orb1t/fly-video-tool.git
+2. Set up at least "videoDir" and "videoTmpDir" properly in "ua.orb1t.wrx.Config" Class
+3. "Run Main" directly from "Project View" or from "Maven View" -> "Plugins" -> "exec" -> "exec:java" 
--- src/main/java/ua/orb1t/wrx/Config.java
+++ src/main/java/ua/orb1t/wrx/Config.java
@@ -6,9 +6,9 @@ import java.nio.file.Path;
 public class Config {
 
     public static final Path pathToFFmpeg    = FileSystems.getDefault().getPath(".\\bin");
-    public static final String  videoDir        = "data/";
+    public static final String  videoDir        = "D:\\_data_\\";
     public static final String  videoTmpDir     = videoDir + "tmp/";
-    public static final String  videoOutDir     = videoDir + "out/";
+    public static final String  videoOutDir     = "data/" + "out/";
     public static final long outputDurationMillisec = 500;
 
     public static final String extension = ".mp4";
